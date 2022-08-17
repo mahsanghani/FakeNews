4f03f72c05a3302f1f8c95873aa89d44a962f1ff
mahsanghani <ghaniahsan@outlook.com>
2022-08-17 15:35:50 -0700

    prediction

--- prediction.py
+++ prediction.py
@@ -1,13 +1,16 @@
-import pickle
+import bz2
+import pickle5 as pickle
 
 var = input("Text: ")
 print("Confirm: " + str(var))
 
-
 # prediction function
 def detecting_fake_news(var):
-# best model
-    load_model = pickle.load(open('final_model.sav', 'rb'))
+    # best model
+    ifile = bz2.BZ2File("model.pkl",'rb')
+    load_model = pickle.load(ifile)
+    ifile.close()
+    # load_model = pickle.load(open('final_model.sav', 'rb'))
     prediction = load_model.predict([var])
     prob = load_model.predict_proba([var])
 
